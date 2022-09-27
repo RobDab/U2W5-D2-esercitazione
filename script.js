3755e5a882ea0e4866e0cebd2380f1069464c76a
RobDab <roberto.dabb@gmail.com>
2022-09-27 18:59:24 +0200

    tentativo da input in html

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
-let initialBudget = 0; 
-console.log(initialBudget)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let initialBudget = 0;
+let newBudget = initialBudget;
 
 function setBudget(){
-    let budget = document.getElementById('initialBudget').value 
-    initialBudget += budget
+    initialBudget += Number(document.getElementById('initialBudget').value);
+    console.log(initialBudget);
 }
+
+
+
+let breakPoint1 = (initialBudget / 2)
+let breakPoint2 = (initialBudget * 0.05)
+let i=0;
+
+while(newBudget > breakPoint1 && i<5){
+    showNew();
+    i++;
+}
+
+
+
+function showNew() {
+    let taken = Number(document.getElementById('take').value);
+    newBudget = newBudget - taken;
+    document.getElementById('still').innerHTML = newBudget
+    let clicked = true
+    return clicked
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
